8e8bb06c6b9e3b9ab1e3577dc1326a7d1d5d50e9
nasiralibaloch <70817789+nasiralibaloch@users.noreply.github.com>
2020-10-17 22:25:34 +0500

    Add files via upload

---
+++ function.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int counter;
+	for(counter=1;counter<=10;counter++)
+	{
+		if(counter==5)
+		
+		cout<<counter<<endl;
+		
+	}
+	return 0;
+}
